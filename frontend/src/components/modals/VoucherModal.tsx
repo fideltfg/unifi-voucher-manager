dacd6a9a5c5b5ce4ccf9f7109e5b5c6f0acd837c
etiennecollin <collin.etienne.contact@gmail.com>
2025-09-07 15:54:24 -0400

    refactor: voucher cards and voucher status

--- frontend/src/components/modals/VoucherModal.tsx
+++ frontend/src/components/modals/VoucherModal.tsx
@@ -3,15 +3,17 @@
 import Modal from "@/components/modals/Modal";
 import Spinner from "@/components/utils/Spinner";
 import { api } from "@/utils/api";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import {
   formatBytes,
   formatDuration,
   formatGuestUsage,
   formatSpeed,
+  formatStatus,
 } from "@/utils/format";
 import VoucherCode from "@/components/utils/VoucherCode";
 import { Voucher } from "@/types/voucher";
+import { TriState } from "@/types/state";
 
 type Props = {
   voucher: Voucher;
@@ -20,8 +22,7 @@ type Props = {
 
 export default function VoucherModal({ voucher, onClose }: Props) {
   const [details, setDetails] = useState<Voucher | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [state, setState] = useState<TriState | null>(null);
   const lastFetchedId = useRef<string | null>(null);
 
   useEffect(() => {
@@ -31,70 +32,81 @@ export default function VoucherModal({ voucher, onClose }: Props) {
     }
 
     (async () => {
-      setLoading(true);
-      setError(false);
+      setState("loading");
       lastFetchedId.current = voucher.id;
 
       try {
         const res = await api.getVoucherDetails(voucher.id);
         setDetails(res);
+        setState("ok");
       } catch {
-        setError(true);
-      } finally {
-        setLoading(false);
+        setState("error");
       }
     })();
   }, [voucher.id]);
 
+  const renderContent = useCallback(() => {
+    switch (state) {
+      case null:
+      case "loading":
+        return <Spinner />;
+      case "error":
+        return (
+          <div className="card text-status-danger text-center">
+            Failed to load detailed information
+          </div>
+        );
+      case "ok":
+        if (details == null) {
+          return;
+        }
+        return (
+          <div className="space-y-4">
+            {(
+              [
+                ["Status", formatStatus(details.expired, details.activatedAt)],
+                ["Name", details.name || "No note"],
+                ["Created", details.createdAt],
+                ...(details.activatedAt
+                  ? [["Activated", details.activatedAt]]
+                  : []),
+                ...(details.expiresAt ? [["Expires", details.expiresAt]] : []),
+                ["Duration", formatDuration(details.timeLimitMinutes)],
+                [
+                  "Guest Usage",
+                  formatGuestUsage(
+                    details.authorizedGuestCount,
+                    details.authorizedGuestLimit,
+                  ),
+                ],
+                [
+                  "Data Limit",
+                  details.dataUsageLimitMBytes
+                    ? formatBytes(details.dataUsageLimitMBytes * 1024 * 1024)
+                    : "Unlimited",
+                ],
+                ["Download Speed", formatSpeed(details.rxRateLimitKbps)],
+                ["Upload Speed", formatSpeed(details.txRateLimitKbps)],
+                ["ID", details.id],
+              ] as [string, any][]
+            ).map(([label, value]) => (
+              <div
+                key={label}
+                className="flex-center-between p-4 bg-interactive border border-subtle rounded-xl space-x-4"
+              >
+                <span className="font-semibold text-primary">{label}:</span>
+                <span className="text-secondary">{value}</span>
+              </div>
+            ))}
+          </div>
+        );
+    }
+  }, [state, details]);
+
   return (
     <Modal onClose={onClose}>
       <VoucherCode voucher={voucher} contentClassName="mb-8" />
-      {loading ? (
-        <Spinner />
-      ) : error || details == null ? (
-        <div className="card text-status-danger text-center">
-          Failed to load detailed information
-        </div>
-      ) : (
-        <div className="space-y-4">
-          {(
-            [
-              ["Status", details.expired ? "Expired" : "Active"],
-              ["Name", details.name || "No note"],
-              ["Created", details.createdAt],
-              ...(details.activatedAt
-                ? [["Activated", details.activatedAt]]
-                : []),
-              ...(details.expiresAt ? [["Expires", details.expiresAt]] : []),
-              ["Duration", formatDuration(details.timeLimitMinutes)],
-              [
-                "Guest Usage",
-                formatGuestUsage(
-                  details.authorizedGuestCount,
-                  details.authorizedGuestLimit,
-                ),
-              ],
-              [
-                "Data Limit",
-                details.dataUsageLimitMBytes
-                  ? formatBytes(details.dataUsageLimitMBytes * 1024 * 1024)
-                  : "Unlimited",
-              ],
-              ["Download Speed", formatSpeed(details.rxRateLimitKbps)],
-              ["Upload Speed", formatSpeed(details.txRateLimitKbps)],
-              ["ID", details.id],
-            ] as [string, any][]
-          ).map(([label, value]) => (
-            <div
-              key={label}
-              className="flex-center-between p-4 bg-interactive border border-subtle rounded-xl space-x-4"
-            >
-              <span className="font-semibold text-primary">{label}:</span>
-              <span className="text-secondary">{value}</span>
-            </div>
-          ))}
-        </div>
-      )}
+      {renderContent()}
     </Modal>
   );
 }
