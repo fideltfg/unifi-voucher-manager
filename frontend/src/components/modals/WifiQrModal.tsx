d14e94056359ff5e3b53a461eb95940a619e29ed
etiennecollin <collin.etienne.contact@gmail.com>
2025-09-07 14:34:51 -0400

    refactor: made WiFi Qr code component modular

--- frontend/src/components/modals/WifiQrModal.tsx
+++ frontend/src/components/modals/WifiQrModal.tsx
@@ -1,69 +1,24 @@
 "use client";
 
-import { useRef, useState, useEffect } from "react";
 import Modal from "@/components/modals/Modal";
-import { QRCodeSVG } from "qrcode.react";
-import { useGlobal } from "@/contexts/GlobalContext";
+import WifiQr from "@/components/utils/WifiQr";
 
 type Props = {
   onClose: () => void;
 };
 
 export default function WifiQrModal({ onClose }: Props) {
-  const modalRef = useRef<HTMLDivElement | null>(null);
-  const [qrSize, setQrSize] = useState(220);
-  const { wifiConfig, wifiString } = useGlobal();
-
-  useEffect(() => {
-    function updateSize() {
-      if (modalRef.current) {
-        // Make sure the QR code scales with the size of the modal
-        const modalWidth = modalRef.current.offsetWidth;
-        const modalHeight = modalRef.current.offsetHeight;
-        const sizeFromWidth = modalWidth * 0.8;
-        const sizeFromHeight = modalHeight * 0.8;
-        setQrSize(Math.floor(Math.min(sizeFromWidth, sizeFromHeight)));
-      }
-    }
-    updateSize();
-
-    window.addEventListener("resize", updateSize);
-    return () => window.removeEventListener("resize", updateSize);
-  }, []);
-
   return (
-    <Modal ref={modalRef} onClose={onClose} contentClassName="max-w-md">
+    <Modal onClose={onClose} contentClassName="max-w-md">
       <div className="flex-center flex-col gap-4">
         <h2 className="text-2xl font-bold text-primary text-center">
-          Wi‑Fi QR Code
+          Wi-Fi QR Code
         </h2>
-        {wifiConfig && wifiString ? (
-          <>
-            <QRCodeSVG
-              value={wifiString}
-              size={qrSize}
-              level="H"
-              bgColor="transparent"
-              fgColor="currentColor"
-              marginSize={4}
-              title="Wi-Fi Access QR Code"
-              imageSettings={{
-                src: "/unifi.svg",
-                height: qrSize / 4,
-                width: qrSize / 4,
-                excavate: true,
-              }}
-            />
-            <p className="text-sm text-muted">
-              Scan this QR code to join the network:{" "}
-              <strong>{wifiConfig.ssid}</strong>
-            </p>
-          </>
-        ) : (
-          <p className="text-sm text-muted text-center">
-            No Wi‑Fi credentials configured.
-          </p>
-        )}
+        <WifiQr className="w-full h-72" sizeRatio={0.88} />
+
+        <p className="text-sm text-muted text-center">
+          Scan this QR code to join the network
+        </p>
       </div>
     </Modal>
   );
